2ba9f38c98f519ced35f21adc470c7822549ec9e
naveen27072004 <naveenlatha2772004@gmail.com>
2024-11-27 19:19:25 +0530

    image showing issue fixed

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const appindex = require('./app')
 const connectDB = require('./config/dbconfig')
 const app = express()
 const cors = require('cors')
+const path = require('path')
 connectDB()
 app.use(cors())
 app.use(express.json())
 
 app.use('/api',appindex)
+app.use('/uploads',express.static(path.join(__dirname,'uploads')))
 
 
 PORT = 8000
