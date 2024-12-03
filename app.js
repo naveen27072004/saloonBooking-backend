9c8466eb79fe813eca113b9e8e202ffb2b35f2c8
naveen27072004 <naveenlatha2772004@gmail.com>
2024-12-03 21:40:27 +0530

    added a partner route

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express()
 const router = express.Router()
 const user_route = require('./routes/user_route')
 const company_route = require('./routes/company_route')
+const partner_route = require('./routes/partner_route')
 
 app.use('/user',user_route)
 app.use('/company',company_route)
+app.use('/partner',partner_route)
 
 module.exports = app
 
